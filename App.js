03927fa2e8f3bd9bffe1f17f321bc9aca6f7854c
TJayashree <jsanthoshithota@gmail.com>
2023-01-11 14:30:40 -0600

    1. basics

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement("h1", {
+    id:"title",
+    className: "class",
+    style: ""
+}, "Namaste Everyone!"); //tag, object, what to put in the tag (type, props, children)
+
+const heading1 = React.createElement("h1", {
+    id:"title",
+}, "Heading1"); 
+const heading2 = React.createElement("h2", {
+    id:"title",
+}, "Heading2"); 
+const container = React.createElement("div",{
+    id: "container",
+},[heading1,heading2]);
+// get the root
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));  //where I want the root to insert
+// put the element into the root
+root.render(container);
