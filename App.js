db4dbf5467502145eb80e88c0731f0175a4c477c
TJayashreeS1412 <jayashree1412@gmail.com>
2025-06-01 22:11:29 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react"; //refers to react inside our node modules.
+import ReactDOM from "react-dom/client";
+
+// this is core of react, but we use JSX instead of this to make it easy
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1", key: "child1" },
+    [
+      React.createElement("h1", { key: "child1h1" }, "I'm an h1 tag"),
+      React.createElement("h2", { key: "child1h2" }, "I'm an h2 tag"),
+    ] //two elemenrts as children, create an array
+  ),
+  React.createElement(
+    "div",
+    { id: "child2", key: "child2" },
+    [
+      React.createElement("h1", { key: "child2h1" }, "I'm an h1 tag"),
+      React.createElement("h2", { key: "child2h2" }, "I'm an h2 tag"),
+    ] //two elemenrts as children, create an array
+  ),
+]);
+
+// get the root // React only works in the place where you insert it so it can be used for any part in the project.
+const root = ReactDOM.createRoot(document.getElementById("root")); //where I want the root to insert
+
+// put the element into the root
+root.render(parent);
